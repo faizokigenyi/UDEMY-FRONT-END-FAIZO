db3b931e461b881b4ba0c4a85b893acf32065e1b
Faizo Kigenyi <faizokigenyi@gmail.com>
2023-11-29 11:19:38 -0600

    Initial commit of whole file

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Player from "./assets/components/Player";
+import { useState } from "react";
+
+function App() {
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name={"Player 1"} symbol = {"X"}/>
+          <Player name={"Player 2"} symbol = {"O"}/>
+        </ol>
+
+      </div>
+
+      LOG
+    </main>
+  )
+}
+
+export default App
